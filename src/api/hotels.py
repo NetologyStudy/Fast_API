960aa8e988d5ac57cccab8f13c16cd22b1d4baa4
Maxim Starichikhin <maks.mayer.ru@outlook.com>
2026-01-03 14:25:13 +0300

    create src/ folder

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,7 +1,7 @@
 from fastapi import Query, Path, APIRouter, Body
 
-from dependencies import PaginationDep
-from schemas.hotels import Hotel, HotelPATCH
+from src.api.dependencies import PaginationDep
+from src.schemas.hotels import Hotel, HotelPATCH
 
 router = APIRouter(prefix="/hotels", tags=["Отили"])
 
