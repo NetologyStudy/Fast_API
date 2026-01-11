835f89d27c8ad4aabbdd5da4095a6a4948419259
Maxim Starichikhin <maks.mayer.ru@outlook.com>
2026-01-11 20:32:02 +0300

    create AuthService

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,37 +1,13 @@
-from datetime import datetime, timedelta, timezone
 import sqlalchemy
 from fastapi import APIRouter, Body, HTTPException, Response
 
-from pwdlib import PasswordHash
-import jwt
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
-
-
-password_hash = PasswordHash.recommended()
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-def verify_password(plain_password, hashed_password):
-    return password_hash.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode |= ({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-def get_hashed_password(password: str) -> str:
-    return password_hash.hash(password)
 
 
 @router.post(
@@ -56,7 +32,7 @@ async def register_user(data: UserRequestAdd = Body(openapi_examples={
     }},
 })
 ):
-    hashed_password = get_hashed_password(data.password)
+    hashed_password = AuthService().get_hashed_password(data.password)
     new_user_data = UserAdd(first_name=data.first_name,
                             last_name=data.last_name,
                             nickname=data.nickname,
@@ -85,8 +61,8 @@ async def login_user(
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
             raise HTTPException(status_code=401, detail="Полтзователь с таким email не зарегистрирован")
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль неверный")
-        access_token = create_access_token({"user_id": user.id})
+        access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
---
+++ src/services/auth.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta, timezone
+
+from pwdlib import PasswordHash
+import jwt
+
+from src.config import settings
+
+
+class AuthService:
+    password_hash = PasswordHash.recommended()
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.password_hash.verify(plain_password, hashed_password)
+
+    def create_access_token(self, data: dict) -> str:
+        to_encode = data.copy()
+        expire = datetime.now(timezone.utc) + timedelta(minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES)
+        to_encode |= ({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+        return encoded_jwt
+
+    def get_hashed_password(self, password: str) -> str:
+        return self.password_hash.hash(password)
