609b1f55912fa649ad3a1449b37734b48df4d40d
Maxim Starichikhin <maks.mayer.ru@outlook.com>
2026-02-02 15:39:10 +0300

    create DataMapper and HotelMapper

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -5,37 +5,14 @@ from sqlalchemy import select
 from src.models.hotels import HotelsOrm
 from src.models.rooms import RoomsOrm
 from src.repositories.base import BaseRepository
+from src.repositories.mappers.mappers import HotelDataMapper
 from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.hotels import Hotel
 
 
 class HotelsRepository(BaseRepository):
     model = HotelsOrm
-    schema = Hotel
-
-
-    async def get_all(
-            self,
-            location,
-            title,
-            stars,
-            limit,
-            offset
-    ):
-        query = select(HotelsOrm)
-        if location:
-            query = query.filter(HotelsOrm.location.icontains(location.strip()))
-        if title:
-            query = query.filter(HotelsOrm.title.icontains(title.strip()))
-        if stars:
-            query = query.filter(HotelsOrm.stars.contains(stars.strip()))
-        query = (
-            query
-            .limit(limit)
-            .offset(offset)
-        )
-        result = await self.session.execute(query)
-        return [Hotel.model_validate(model, from_attributes=True) for model in result.scalars().all()]
+    mapper = HotelDataMapper
 
 
     async def get_filtered_by_time(
@@ -70,4 +47,4 @@ class HotelsRepository(BaseRepository):
         )
         result = await self.session.execute(query)
 
-        return [Hotel.model_validate(model, from_attributes=True) for model in result.scalars().all()]
+        return [self.mapper.map_to_domain_entity(hotel) for hotel in result.scalars().all()]
