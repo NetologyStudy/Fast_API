1c4d02a08050dfdac03d3197b8e6921290cc534b
Maxim Starichikhin <maks.mayer.ru@outlook.com>
2026-02-03 12:41:21 +0300

    add all other data mappers

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -5,13 +5,13 @@ from sqlalchemy.orm import selectinload
 
 from src.models.rooms import RoomsOrm
 from src.repositories.base import BaseRepository
+from src.repositories.mappers.mappers import RoomDataMapper, RoomWithRelsDataMapper
 from src.repositories.utils import rooms_ids_for_booking
-from src.schemas.rooms import Room, RoomWithRels
 
 
 class RoomsRepository(BaseRepository):
     model = RoomsOrm
-    schema = Room
+    mapper = RoomDataMapper
 
 
     async def get_filtered_by_time(
@@ -33,7 +33,7 @@ class RoomsRepository(BaseRepository):
             .filter(RoomsOrm.id.in_(rooms_ids_to_get))
         )
         result = await self.session.execute(query)
-        return [RoomWithRels.model_validate(model, from_attributes=True) for model in result.scalars().all()]
+        return [RoomWithRelsDataMapper.map_to_domain_entity(model) for model in result.scalars().all()]
 
 
     async def get_one_or_none_with_rels(self, **filter_by):
@@ -45,4 +45,4 @@ class RoomsRepository(BaseRepository):
         model = result.scalars().one_or_none()
         if model is None:
             return None
-        return RoomWithRels.model_validate(model, from_attributes=True)
+        return RoomWithRelsDataMapper.map_to_domain_entity(model)
